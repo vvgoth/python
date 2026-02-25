6d3ee3d19923c3c50125d9a273c9a4fade0ecbe0
Bilge Kutay <blgkty10@gmail.com>
2026-02-25 19:05:44 +0300

    Update sine wave plot to include vertical shift in y-axis label

--- CE580/hw1.py
+++ CE580/hw1.py
@@ -3,11 +3,11 @@ import numpy as np
 
 # example plot
 x = np.linspace(0, 10, 100)
-y = np.sin(x)
+y = np.sin(x) + 1
 
 plt.plot(x, y)
 plt.title('Sine Wave')
 plt.xlabel('x')
-plt.ylabel('sin(x)')
+plt.ylabel('sin(x) + 1')
 plt.grid()
 plt.show()
